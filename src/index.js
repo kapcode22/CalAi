1883e9fa826f7312810bd46032f1768355baa105
iamkapil <kapilrhngdle2211@gmail.com>
2024-04-14 22:54:12 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-25GE0F0K5K");
+ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-    <App />
+      <App />
     </Router>
   </React.StrictMode>
 );
